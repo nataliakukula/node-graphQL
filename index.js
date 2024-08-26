bb3771a785c53d89b2085ed3c5460d6cc621aeb7
Kinky Cookie <kuki_kukula@yahoo.com>
2024-08-26 16:17:55 -0500

    schema

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema.js';
+import db from './_db.js';
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        }
+    }
+};
+
+// server setup
+const server = new ApolloServer({
+typeDefs,
+resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 5000 }
+});
+
+console.log('server ready at port 5000');
