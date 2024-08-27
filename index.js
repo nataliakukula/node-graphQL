a0adc13f67384a8eec8441d09e9aa67ecfc0f6cf
Kinky Cookie <kuki_kukula@yahoo.com>
2024-08-27 17:32:01 -0500

    mutations

--- index.js
+++ index.js
@@ -5,44 +5,70 @@ import db from './_db.js';
 
 const resolvers = {
     Query: {
-        games() {
-            return db.games
-        },
-        reviews() {
-            return db.reviews
-        },
-        authors() {
-            return db.authors
-        },
-        review(_, args) {
-            return db.reviews.find((rev) => rev.id === args.id)
-        },
-        game(_, args) {
-            return db.games.find((g) => g.id === args.id)
-        },
-        author(_, args) {
-            return db.authors.find((au) => au.id === args.id)
-        }
+      games() {
+        return db.games
+      },
+      game(_, args) {
+        return db.games.find((game) => game.id === args.id)
+      },
+      authors() {
+        return db.authors
+      },
+      author(_, args) {
+        return db.authors.find((author) => author.id === args.id)
+      },
+      reviews() {
+        return db.reviews
+      },
+      review(_, args) {
+        return db.reviews.find((review) => review.id === args.id)
+      }
     },
     Game: {
-        reviews(parent) {
-            return db.reviews.filter((r) => r.game_id === parent.id)
-        }
-    },
-    Author: {
-        reviews(parent) {
-            return db.reviews.filter((r) => r.author_id === parent.id)
-        }
+      reviews(parent) {
+        return db.reviews.filter((r) => r.game_id === parent.id)
+      }
     },
     Review: {
-        author(parent) {
-            return db.authors.filter((a) => a.id === parent.author_id)
+      author(parent) {
+        return db.authors.find((a) => a.id === parent.author_id)
+      },
+      game(parent) {
+        return db.games.find((g) => g.id === parent.game_id)
+      }
+    },
+    Author: {
+      reviews(parent) {
+        return db.reviews.filter((r) => r.author_id === parent.id)
+      }
+    },
+    Mutation: {
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+
+            db.games.push(game);
+
+            return game;
         },
-        game(parent) {
-            return db.games.filter((g) => g.id === parent.game_id)
+        deleteGame(_, args) {
+            db.games = db.games.filter((g)=> g.id !== args.id);
+
+            return db.games;
+        },
+        updateGame(_, args) {
+            db.games = db.games.map((g)=> {
+                if (g.id === args.id) {
+                    return {...g, ...args.edits}
+                }
+
+                return g;
+            })
         }
     }
-};
+  }
 
 // server setup
 const server = new ApolloServer({
